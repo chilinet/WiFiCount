b85d61df94e99de7b97aaa96c7555e5a51e87b18
chilinet <sluther@chilinet.solutions>
2025-04-18 17:04:18 +0200

    Umstellung auf MSSQL + Build 4

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
+  distDir: '.next',
+  poweredByHeader: false,
+  env: {
+    PORT: process.env.PORT || 3000
+  }
 }
 
 module.exports = nextConfig 
